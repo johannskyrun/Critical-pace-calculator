7eece5f42ef52a12d1ff370665f5d30fbdfcd9cb
johannskyrun <132217232+johannskyrun@users.noreply.github.com>
2025-01-17 16:16:11 +0200

    critical_pace_calculator.py

--- critical_pace_calculator.py
+++ critical_pace_calculator.py
@@ -231,7 +231,7 @@ def main():
     st.write("www.switchback-coaching.com")
     st.write("Enter your 400m, 800m, and 5000m times. Use the 'mm:ss' format e.g. 1:41")
     st.write("Use your times from the last 16 weeks to get a estimation of your current ability or realistic times for your current ability")
-     st.write("www.switchback-coachin.com")
+    
 
     # Inputs for times
     time_400 = st.text_input("400m Time", value="1:30")
